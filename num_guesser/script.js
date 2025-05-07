0bbae0b0984a60dac6da84c8403aa760e682f66e
kamilszt00 <kamil.sz00@proton.me>
2025-05-07 06:21:28 -0700

    started num guesser project, added simple comparing function, random number function and score updating func

---
+++ num_guesser/script.js
@@ -0,0 +1,23 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => Math.floor(Math.random() * 10)
+
+function compareGuesses(user,computer,secret) {
+    return (Math.abs(secret - user) <= (Math.abs(secret - computer))) ? true : false;
+        
+}
+
+const updateScore = (win) => {
+    if (win.toLowerCase() === 'human') {
+        humanScore ++;
+        console.log(humanScore)
+    } else if (win.toLowerCase() === 'computer') {
+        computerScore ++;
+    }
+
+}
+
+
